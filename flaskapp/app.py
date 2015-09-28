7439176770c88e5e92d67fce1d60b1193d63ebcd
Benson Mburu <nguruben@gmail.com>
2015-09-28 15:03:28 +0300

    renderd template in python

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -7,3 +7,9 @@ def main():
     return "Welcome!"
 if __name__ == "__main__":
     app.run()
+    
+    #import render template
+    from flask import Flask, render_template
+    #modified the main methodto return the renderd template
+    def main():
+        return render_template('index.html')
