bec37d7b81c6272164e2d2bfd332e716aa73c1a1
Benson Mburu <nguruben@gmail.com>
2015-09-28 16:45:13 +0300

    added json validation

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 #define the basic route
  #import render template
-from flask import Flask, render_template, request
+from flask import Flask, render_template, json, request
     #modified the main method to return the renderd template
 @app.route("/")
 def main():
@@ -17,7 +17,11 @@ def signup():
     _name = request.form['inputName']
     _email -request.from['inputEmail']
     _password =request.form['inputPassword']
-    
+    #Validae the recieved values:
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
     
 if __name__ == "__main__":
     app.run()
