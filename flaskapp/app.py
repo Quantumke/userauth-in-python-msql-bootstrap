1e7f5cb289f1eb3afcbb95c6bb0b011e7ed55ec9
Benson Mburu <nguruben@gmail.com>
2015-09-28 16:37:58 +0300

    GUI render from mysql

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 #define the basic route
  #import render template
-from flask import Flask, render_template
+from flask import Flask, render_template, request
     #modified the main method to return the renderd template
 @app.route("/")
 def main():
@@ -11,6 +11,13 @@ def main():
 @app.route('/showSignUp')
 def showSignUp():
      return render_template('signup.html')
+@app.route('/signup' methods=['POST'])
+def signup():
+    #read the posted values
+    _name = request.form['inputName']
+    _email -request.from['inputEmail']
+    _password =request.form['inputPassword']
+    
     
 if __name__ == "__main__":
     app.run()
